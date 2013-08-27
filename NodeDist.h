fe104829f16a4c373aab5a89adf4735c87d9014b
EdlinLink <edlinlink@qq.com>
2013-08-27 17:51:35 +0800

    not complete yet

---
+++ NodeDist.h
@@ -0,0 +1,11 @@
+#ifndef NODEDIST_H
+#define NODEDIST_H
+
+
+class NodeDist{
+public:
+	long name;
+	long dist;
+};
+
+#endif
---
+++ Query.h
@@ -0,0 +1,12 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+
+class Query{
+public:
+	long a;
+	long b;
+	long limit;
+	int label;
+};
+#endif
